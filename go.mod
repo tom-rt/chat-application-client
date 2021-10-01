9ece8dca797a251b8ffce1765c3cfc11527b1e5b
Thomas Raout <thomraout@gmail.com>
2021-10-02 00:05:40 +0200

    WIP connection and basic communication with server working

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module chat-application/client
 
 go 1.17
+
+require github.com/gorilla/websocket v1.4.2
--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/gorilla/websocket"
 )
+
+type messageStruct struct {
+	Connection    bool
+	Disconnection bool
+	Nickname      string
+	Message       string
+}
 
 func parseFlags() (string, string, error) {
 	var nickname string
@@ -25,6 +40,85 @@ func parseFlags() (string, string, error) {
 	return nickname, host + ":" + port, nil
 }
 
+func handleSession(serverAddress string, nickname string) {
+	messageUrl := url.URL{Scheme: "ws", Host: serverAddress, Path: "/run/session"}
+	interrupt := make(chan os.Signal, 1)
+
+	messageDialer, _, err := websocket.DefaultDialer.Dial(messageUrl.String(), nil)
+	if err != nil {
+		log.Fatal("Error dialing:", err)
+	}
+	defer messageDialer.Close()
+
+	stdio := make(chan string)
+
+	// CONNECTING
+	connMessage := &messageStruct{Connection: true, Disconnection: false, Nickname: nickname, Message: "connecting !"}
+	connJson, error := json.Marshal(connMessage)
+	if error != nil {
+		fmt.Println("Error while marshalling", error)
+	}
+	err = messageDialer.WriteMessage(websocket.TextMessage, []byte(string(connJson)))
+	if err != nil {
+		log.Println("Error writing message.", err)
+		return
+	}
+
+	_, connectionResponse, err := messageDialer.ReadMessage()
+	if err != nil {
+		log.Println("Error on reading:", err)
+		return
+	}
+
+	log.Printf("Connection reponse: %s", connectionResponse)
+
+	// READING INCOMING MESSAGES FROM MESSAGE DIALER
+	go func() {
+		for {
+			_, message, err := messageDialer.ReadMessage()
+			if err != nil {
+				log.Println("Read error:", err)
+				return
+			}
+			log.Printf("Received: %s", message)
+		}
+	}()
+
+	// READING STANDARD INPUT
+	go func() {
+		for {
+			var reader = bufio.NewReader(os.Stdin)
+			message, _ := reader.ReadString('\n')
+
+			stdio <- message
+		}
+	}()
+
+	for {
+		select {
+		case msg := <-stdio:
+
+			payload := &messageStruct{Connection: false, Disconnection: false, Nickname: nickname, Message: strings.Trim(msg, "\n")}
+			marshaledPayload, error := json.Marshal(payload)
+			if error != nil {
+				fmt.Println("error marshalling", error)
+			}
+			err = messageDialer.WriteMessage(websocket.TextMessage, []byte(string(marshaledPayload)))
+			if err != nil {
+				log.Println("Error writing message.", err)
+				return
+			}
+
+		case <-interrupt:
+			err = messageDialer.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "Closing client"))
+			if err != nil {
+				log.Println("Error on closing client:", err)
+			}
+			return
+		}
+	}
+}
+
 func main() {
 	nickname, serverAddress, err := parseFlags()
 
@@ -35,4 +129,5 @@ func main() {
 	fmt.Println("your nickname is:", nickname)
 	fmt.Println("server address:", serverAddress)
 
+	handleSession(serverAddress, nickname)
 }
